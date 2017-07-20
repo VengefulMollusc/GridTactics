2620cdff3a82f0baf3ea14029b3c76982a6e02ee
Chris Read <VengefulMollusc@users.noreply.github.com>
2017-07-20 15:51:54 +1200

    finished abilitymenuentry script

--- Assets/Scripts/Component/AbilityMenuEntry.cs
+++ Assets/Scripts/Component/AbilityMenuEntry.cs
@@ -43,4 +43,53 @@ public class AbilityMenuEntry : MonoBehaviour {
         }
     }
 
+    public bool IsSelected
+    {
+        get { return (State & States.Selected) != States.None; }
+        set
+        {
+            if (value)
+                State |= States.Selected;
+            else
+                State &= ~States.Selected;
+        }
+    }
+
+    private States State
+    {
+        get { return state; }
+        set
+        {
+            if (state == value)
+                return;
+            state = value;
+
+            if (IsLocked)
+            {
+                bullet.sprite = disabledSprite;
+                label.color = Color.gray;
+                outline.effectColor = new Color32(20, 36, 44, 255);
+            }
+            else if (IsSelected)
+            {
+                bullet.sprite = selectedSprite;
+                label.color = new Color32(255, 210, 118, 255);
+                outline.effectColor = new Color32(255, 160, 72, 255);
+            }
+            else
+            {
+                bullet.sprite = normalSprite;
+                label.color = Color.white;
+                outline.effectColor = new Color32(20, 36, 44, 255);
+            }
+        }
+    }
+
+    private States state;
+
+    public void Reset()
+    {
+        State = States.None;
+    }
+
 }

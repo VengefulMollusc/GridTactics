7e1c3cfd5ad1fef905a9acdb78cc3e74bb3d76a5
Chris Read <VengefulMollusc@users.noreply.github.com>
2017-07-31 17:43:08 +1200

    completed equippable implementation

---
+++ Assets/Scripts/Component/Item/Equippable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equippable : MonoBehaviour
+{
+
+    public EquipSlots defaultSlots;
+    public EquipSlots secondarySlots; // allows for equipping in an alternate slot (eg dual-wield swords)
+    public EquipSlots slots;
+    private bool _isEquipped;
+
+    public void OnEquip()
+    {
+        if (_isEquipped)
+            return;
+
+        _isEquipped = true;
+
+        Feature[] features = GetComponentsInChildren<Feature>();
+        for (int i = 0; i < features.Length; ++i)
+            features[i].Activate(gameObject);
+    }
+
+    public void OnUnEquip()
+    {
+        if (!_isEquipped)
+            return;
+
+        _isEquipped = false;
+
+        Feature[] features = GetComponentsInChildren<Feature>();
+        for (int i = 0; i < features.Length; ++i)
+            features[i].Deactivate();
+
+    }
+}

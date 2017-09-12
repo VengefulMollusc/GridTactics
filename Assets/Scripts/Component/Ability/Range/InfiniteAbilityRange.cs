382af9066da304db04e4f7dfc7ba42938863e70b
Chris Read <VengefulMollusc@users.noreply.github.com>
2017-09-12 16:55:56 +1200

    ability range complete

---
+++ Assets/Scripts/Component/Ability/Range/InfiniteAbilityRange.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteAbilityRange : AbilityRange {
+    public override List<Tile> GetTilesInRange(Board board)
+    {
+        return new List<Tile>(board.tiles.Values);
+    }
+}

55dcc30f9099560f17ed5b5a02bfff7723096d51
Chris Read <VengefulMollusc@users.noreply.github.com>
2017-09-15 16:16:00 +1200

    finished durationstatuscondition

---
+++ Assets/Scripts/Component/Status/Conditions/DurationStatusCondition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DurationStatusCondition : StatusCondition
+{
+
+    public int duration = 10;
+
+    void OnEnable()
+    {
+        this.AddObserver(OnNewTurn, TurnOrderController.RoundBeganNotification);
+    }
+
+    void OnDisable()
+    {
+        this.RemoveObserver(OnNewTurn, TurnOrderController.RoundBeganNotification);
+    }
+
+    void OnNewTurn(object sender, object args)
+    {
+        duration--;
+        if (duration <= 0)
+        {
+            Remove();
+        }
+    }
+}
---
+++ Assets/Scripts/Component/Status/Conditions/StatusCondition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class StatusCondition : MonoBehaviour {
+
+    public virtual void Remove()
+    {
+        Status s = GetComponent<Status>();
+        if (s)
+        {
+            s.Remove(this);
+        }
+    }
+}

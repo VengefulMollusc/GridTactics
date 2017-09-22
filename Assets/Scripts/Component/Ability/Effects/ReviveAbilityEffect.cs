098e163fcf63b768cd098c96716bf7abdf5e5e4e
Chris Read <VengefulMollusc@users.noreply.github.com>
2017-09-22 16:22:38 +1200

    Magic-Ability done

---
+++ Assets/Scripts/Component/Ability/Effects/ReviveAbilityEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ReviveAbilityEffect : BaseAbilityEffect
+{
+
+    public float percent;
+
+    public override int Predict(Tile target)
+    {
+        Stats s = target.content.GetComponent<Stats>();
+        return Mathf.FloorToInt(s[StatTypes.MHP] * percent);
+    }
+
+    protected override int OnApply(Tile target)
+    {
+        Stats s = target.content.GetComponent<Stats>();
+        int value = s[StatTypes.HP] = Predict(target);
+        return value;
+    }
+}
